32ec7805f5f250a8e65d0d67446bc77a1fbf9308
STQT <106919829+STQT@users.noreply.github.com>
2024-10-12 19:12:49 +0500

    Update autorun.bat

--- autorun.bat
+++ autorun.bat
@@ -1,4 +1,16 @@
 @echo off
 set "current_dir=%~dp0"
 echo %current_dir%
+
+:wait_for_internet
+echo Check internet...
+ping -n 1 8.8.8.8 >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Internet wasnt found. Recheck after 10 seconds...
+    timeout /t 10 /nobreak >nul
+    goto wait_for_internet
+)
+
+echo Internet connected. Run npm...
+
 npm --prefix %current_dir% start
